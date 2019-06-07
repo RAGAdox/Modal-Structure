088818b02d53d494d6185aa87dc93a7b66541dc9
RAGAdox <rishitech882@gmail.com>
2019-06-07 17:03:18 +0530

    About Us

---
+++ scripts/about.script.js
@@ -0,0 +1,30 @@
+let i = 0,
+  j = 0;
+let txt =
+  "About Me \n Everything I do, I do it because I love it.\nInterested in modern and promising technologies for development .\n A Full-Stack Developer \\ Engineer proficient in Open-Source Cross-Platform Technologies. \n";
+let txt2 = "This will keep on Updating ...";
+let content = document.getElementById("heading");
+let content2 = document.getElementById("subHeading");
+let speed = 50;
+let br = document.createElement("br");
+let typer = () => {
+  console.log("function called");
+
+  if (i < txt.length) {
+    content.innerHTML += txt.charAt(i);
+    if (txt.charAt(i) == "\n") content.appendChild(br);
+    i++;
+    setTimeout(typer, speed);
+  }
+  if (i >= txt.length && i < txt2.length) {
+    typeSmall();
+  }
+};
+typeSmall = () => {
+  if (j < txt2.length) {
+    content2.innerHTML += txt2.charAt(j);
+    if (txt2.charAt(j) == "\n") content2.appendChild(br);
+    j++;
+    setTimeout(typeSmall, speed);
+  }
+};
